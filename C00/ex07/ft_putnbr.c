29c1a45e0acad7701156d1302380f95671764d13
Arthur Gouzy <arthurgouzy@gmail.com>
2024-10-15 01:03:57 +0200

    first commit and completed C00 to 100%

---
+++ C00/ex07/ft_putnbr.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agouzy <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/07 01:56:05 by agouzy            #+#    #+#             */
+/*   Updated: 2024/10/07 02:11:04 by agouzy           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return ;
+	}
+	if (nb < 0)
+	{
+		nb *= -1;
+		ft_putchar('-');
+	}
+	if (nb <= 9)
+	{
+		ft_putchar(nb + '0');
+	}
+	else
+	{
+		ft_putnbr(nb / 10);
+		ft_putchar(nb % 10 + '0');
+	}
+}
+
+/*
+#include <stdlib.h>
+
+int	main(int argc, char *argv[])
+{
+	if (argc == 2)
+		ft_putnbr(atoi(argv[1]));
+	return (0);
+}
+*/
