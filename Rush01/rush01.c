aa60ccc4b9a3747371e6c9938077b669f81c8360
ArthurG24 <gouzyarthur@gmail.com>
2024-11-13 21:11:23 +0100

    wip rush01

---
+++ Rush01/rush01.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush01.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arthur <arthur@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/13 14:57:43 by arthur            #+#    #+#             */
+/*   Updated: 2024/11/13 17:26:25 by arthur           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_check(int line[], int index, int number)
+{
+	int	i;
+
+	i = 0;
+	while (i < index)
+	{
+		if (line[i] == number)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	ft_fill(int line[], int index, int size)
+{
+	int	number;
+
+	number = 1;
+	while (number <= size)
+	{
+		if (ft_check(line, index, number))
+			return (line[index] = number);	
+		number++;
+	}
+}
+
+
+int	main(void)
+{
+	int	line[] = {3, 4, 1};
+	int	size;
+	int	index;
+
+	index = 0;
+	size = 4;
+
+/* 	while (index < size)
+	{
+		ft_fill(line, index, size);
+		index++;
+	} */
+
+	int i = 0;
+	while (i < size)
+	{
+		printf("%d\n", line[i]);
+		i++;
+	}
+}
