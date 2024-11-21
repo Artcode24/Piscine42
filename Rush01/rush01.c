65eccc82a4f349d6031ee07c0a62ae3fe0f7a4ce
ArthurG24 <gouzyarthur@gmail.com>
2024-11-21 16:58:21 +0100

    rush01 completed ft_fill_line

--- Rush01/rush01.c
+++ Rush01/rush01.c
@@ -6,7 +6,7 @@
 /*   By: arthur <arthur@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 14:57:43 by arthur            #+#    #+#             */
-/*   Updated: 2024/11/19 18:07:20 by arthur           ###   ########.fr       */
+/*   Updated: 2024/11/21 16:50:04 by arthur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,12 +27,12 @@ int	ft_check(int line[], int index, int number)
 	return (1);
 }
 
-void	ft_fill_case(int line[], int index, int base)
+void	ft_fill_case(int line[], int index, int min_number)
 {
 	int	number;
 
-	number = base;
-	while (number <= base + 3)
+	number = min_number;
+	while (number <= min_number + 3)
 	{
 		if (ft_check(line, index, (number - 1) % 4 + 1))
 		{
@@ -64,32 +64,48 @@ int	ft_calcul_line(int line[], int array_size)
 	return (view_count);
 }
 
+void	ft_fill_line(int line[], int min_number)
+{
+	int	index;
+
+	index = 0;
+	while (index < 4)
+	{
+		ft_fill_case(line, index, min_number);
+		index++;
+	}
+}
+
 int	main(int argc, char *argv[])
 {
 	int	line[4] = {0, 0, 0, 0};
 	int	index;
-	int	base;
-	int	attendu;
+	int	min_number;
+	int	expected;
 
-
+	// It takes a parameters to check how many boxes we should see
 	if (argc != 2)
 		return (1);
 
-	base = 1;
-	attendu = atoi(argv[1]);
+	expected = atoi(argv[1]);
 
-	// Put it in a function called ft_fill_line
-	while ((ft_calcul_line(line, 4) != attendu) || line[0] == 0)
+	// This loop runs as long as we don't see the correct amount of boxes
+	min_number = 1;
+	while ((ft_calcul_line(line, 4) != expected) || line[0] == 0)
 	{
-		index = 0;
-		while (index < 4)
-		{
-			ft_fill_case(line, index, base);
-			index++;
-		}
-		base++;
+		ft_fill_line(line, min_number);
+		min_number++;
 	}
 
+
+	// Start the loop again while changing at least one element. > ft_line_compare
+	// If it's not possible, go back to the initial state.
+	// Maybe use a copy ? And use a ft_line_compare
+	// Or maybe rather run an inner loop inside the first one, as long as ft_calcul == expected
+
+	// CHANGER L'INDEX APRES LE CHIFFRE LE PLUS GRAND UNE FOIS QUE FT_CALCUL RETOURNE EXPECTED
+	
+	// Print the line to test
 	for (int i = 0; i < 4; i++)
 		printf("%d ", line[i]);
 	printf("\n");
