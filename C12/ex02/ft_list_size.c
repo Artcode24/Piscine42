7cf310fe9e9f0d768282298f794ba53c567e7c10
ArthurG24 <gouzyarthur@gmail.com>
2025-01-07 17:35:19 +0100

    c12ex02

--- C12/ex02/ft_list_size.c
+++ C12/ex02/ft_list_size.c
@@ -6,11 +6,24 @@
 /*   By: arthur <arthur@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 17:28:16 by arthur            #+#    #+#             */
-/*   Updated: 2025/01/07 17:28:25 by arthur           ###   ########.fr       */
+/*   Updated: 2025/01/07 17:34:54 by arthur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_list.h"
+
 int	ft_list_size(t_list *begin_list)
 {
+	t_list	*ptr;
+	int	count;
+
+	ptr = begin_list;
 	
+	count = 0;
+	while (ptr)
+	{
+		count++;
+		ptr = ptr->next;
+	}
+	return (count);
 }
