a38f81c45b373be1dd973477434b52b1d2de9308
Artcode24 <gouzyarthur@gmail.com>
2025-02-02 06:31:07 +0100

    modif ten queens

--- C05/ex08/ft_ten_queens_puzzle.c
+++ C05/ex08/ft_ten_queens_puzzle.c
@@ -6,7 +6,7 @@
 /*   By: arthur <arthur@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/26 00:51:30 by arthur            #+#    #+#             */
-/*   Updated: 2025/01/31 04:18:40 by arthur           ###   ########.fr       */
+/*   Updated: 2025/01/31 07:10:59 by arthur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,139 +14,110 @@
 
 #define SIZE 10
 
-int COUNT = 0;
+int g_grid[10][10];
+int c_count = 0;
 
-void print_grid(int grid[10][10])
+void init_grid(void)
 {
     for (int i = 0; i < SIZE; i++)
     {
         for (int j = 0; j < SIZE; j++)
         {
-            printf("%d ", grid[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-void init_grid(int grid[10][10])
-{
-    for (int i = 0; i < SIZE; i++)
-    {
-        for (int j = 0; j < SIZE; j++)
-        {
-            grid[i][j] = 0;
+            g_grid[i][j] = 0;
         }
     }
 }
 
-int row_is_filled(int row, int grid[10][10])
+int row_is_filled(int row)
 {
     for (int i = 0; i < SIZE; i++)
     {
-        if (grid[row][i] == 1)
+        if (g_grid[row][i] == 1)
             return (1);
     }
     return (0);
 }
 
-int diag_left_is_filled(int row, int col, int grid[10][10])
+int diag_left_is_filled(int row, int col)
 {
 	for (int i = row + 1, j = col + 1; i < SIZE && j < SIZE; i++, j++)
 	{
-		if (grid[i][j] == 1)
+		if (g_grid[i][j] == 1)
 			return (1);
 	}
 
 	for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--)
 	{
-		if (grid[i][j] == 1)
+		if (g_grid[i][j] == 1)
 			return (1);
 	}
 	
 	return (0);
 }
 
-int diag_right_is_filled(int row, int col, int grid[10][10])
+int diag_right_is_filled(int row, int col)
 {
 	for (int i = row - 1, j = col + 1; i < SIZE && j < SIZE; i--, j++)
 	{
-		if (grid[i][j] == 1)
+		if (g_grid[i][j] == 1)
 			return (1);
 	}
 
 	for (int i = row + 1, j = col -1; i < SIZE && j < SIZE; i++, j--)
 	{
-		if (grid[i][j] == 1)
+		if (g_grid[i][j] == 1)
 			return (1);
 	}
 	
 	return (0);
 }
 
-int	find(int grid[10][10], int col)
+void	print_positions()
 {
-	for (int i = 0; i < SIZE; i++)
+	for (int col = 0; col < SIZE; col++)
 	{
-		if(grid[i][col])
+		for (int row = 0; row < SIZE; row++)
 		{
-			return (i);
+			if(g_grid[row][col])
+			{
+				printf("%d", row);
+			}
 		}
 	}
-	return (0);
+	printf("\n");
 }
 
-int solve(int col, int grid[10][10])
+void solve(int col)
 {
     if (col == SIZE) // Toutes les colonnes sont remplies
     {
-	COUNT++;
-	print_grid(grid);
-	printf("\n");
-	for (int i = 0; i < SIZE; i++)
-	{
-		printf("%d", find(grid, i));
-	}
-	printf("\n");
-	printf("\n");
-	printf("\n");
+	c_count++;
+	print_positions();
     }
 
     for (int row = 0; row < SIZE; row++) // Parcourir les lignes
     {
-        if (!row_is_filled(row, grid) && !diag_left_is_filled(row, col, grid) && !diag_right_is_filled(row, col, grid))
+        if (!row_is_filled(row) && !diag_left_is_filled(row, col) && !diag_right_is_filled(row, col))
         {
-            grid[row][col] = 1; // Place une reine à cette position
-            if (solve(col + 1, grid)) // Appel récursif pour la colonne suivante
-                return (1);
-            grid[row][col] = 0; // Backtracking
+		g_grid[row][col] = 1;
+		solve(col + 1);
+		g_grid[row][col] = 0;
         }
     }
-
-    return (0); // Aucun placement possible pour cette colonne
 }
 
-int ft_ten_queens_puzzle(int grid[10][10])
+int ft_ten_queens_puzzle(void)
 {
-	if (solve(0, grid)) // Démarre à la première colonne
-	{
-        	// print_grid(grid);
-	        return (1);
-	}
-	else
-	{
-        	printf("Aucune solution trouvée\n");
-        	return (0);
-    	}
+	solve(0);
+	return (c_count);
 }
 
-int main(void)
+/* 
+int 	main(void)
 {
-	int	grid[SIZE][SIZE];
-	int	grid_copy[SIZE][SIZE];
-	
-	init_grid(grid);
-	
-	ft_ten_queens_puzzle(grid);
-
+	init_grid();
+	int count = ft_ten_queens_puzzle();
+	printf("\n%d\n", count);
 	return (0);
-}
+} 
+ */
