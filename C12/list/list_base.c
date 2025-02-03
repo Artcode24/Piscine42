271ca78835b17c3e1bc138d531dd6d2a4111afc2
ArthurG24 <gouzyarthur@gmail.com>
2025-02-03 11:54:51 +0100

    working on c12ex05

--- C12/list/list_base.c
+++ C12/list/list_base.c
@@ -6,7 +6,7 @@
 /*   By: arthur <arthur@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 16:41:04 by arthur            #+#    #+#             */
-/*   Updated: 2025/02/02 09:37:57 by arthur           ###   ########.fr       */
+/*   Updated: 2025/02/03 11:54:22 by arthur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ t_list	*ft_create_element(void *);
 void	ft_list_push_front(t_list **, void *);
 t_list	*ft_list_last(t_list *);
 void	ft_list_push_back(t_list **, void *);
+t_list	*ft_list_push_strs(int size, char **strs);
 
 
 
@@ -65,13 +66,36 @@ int	ft_list_size(t_list *begin_list)
 	return (count);
 }
 
-void ft_list_push_back(t_list **begin_list, void *data)
-{
-
-}
-
-
 t_list	*ft_list_last(t_list *begin_list)
 {
+	t_list	*ptr;
 
+	ptr = begin_list;
+	while (ptr->next)
+		ptr = ptr->next;
+	return (ptr);
+}
+
+void	ft_list_push_back(t_list **begin_list, void *data)
+{
+	t_list	*new_elem;
+	t_list	*ptr;
+
+	new_elem = ft_create_element(data);
+	ptr = *begin_list;
+	
+	while (ptr->next)	
+	{
+		ptr = ptr->next;
+	}
+	ptr->next = new_elem;
+}
+
+t_list	*ft_list_push_strs(int size, char **strs)
+{
+	char	*strs[] = {"Hello", "Konnichiwa", "Annyeong", "Nihao"};
+	t_list	*begin_list;
+
+	ft_create_element(strs[0])
+	// faire une boucle
 }
